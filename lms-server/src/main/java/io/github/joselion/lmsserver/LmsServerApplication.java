1d8e0132e7458c4f3220a25d182961ad25356e0c
JoseLion <joseluis5000l@gmail.com>
2023-10-29 01:07:24 -0500

    chore(server): Setup server

---
+++ lms-server/src/main/java/io/github/joselion/lmsserver/LmsServerApplication.java
@@ -0,0 +1,12 @@
+package io.github.joselion.lmsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsServerApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(LmsServerApplication.class, args);
+  }
+}
